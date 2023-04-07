8e2447499919f0b064bbccc1e30943c6188ef4fd
Arushivisuto <117741091+Arushivisuto@users.noreply.github.com>
2023-04-07 17:01:38 +0200

    Add files via upload

--- classes/classes_v2/ant_test.py
+++ classes/classes_v2/ant_test.py
@@ -4,8 +4,8 @@ Created on Fri Mar 17 17:51:53 2023
 
 @author: angab
 """
-from IPython import get_ipython
-get_ipython().magic('reset -sf')
+#from IPython import get_ipython
+#get_ipython().magic('reset -sf')
 import classe_mot_t as mo
 import AntenneT as ant
 
