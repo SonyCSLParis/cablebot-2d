af72a42ae8c44d5a29a287b00da3bf60f296a073
Arushivisuto <117741091+Arushivisuto@users.noreply.github.com>
2023-04-07 17:46:40 +0200

    Add files via upload

--- classes/classes_v2/classe_mot_t.py
+++ classes/classes_v2/classe_mot_t.py
@@ -169,7 +169,7 @@ class OdriveMot:
                 time.sleep(0.1)
 
             # Définir l'état actuel de l'axe
-            ax.requested_state = 8
+            ax.requested_state = od.AXIS_STATE_CLOSE_LOOP_CONTROL
             time.sleep(1)
             print("close control")
             
