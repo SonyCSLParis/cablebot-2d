68a21722ddb4363ae66378c8e4541be29787c509
LeoMaurel <120198498+LeoMaurel@users.noreply.github.com>
2023-04-21 15:02:16 +0200

    Add files via upload

---
+++ mainCam.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 17:51:53 2023
+
+@author: angab
+"""
+from IPython import get_ipython
+#get_ipython().magic('reset -sf')
+
+import ClasseCam as ant
+
+if __name__=="__main__":
+    antenne=ant.AntenneCam(15555)
+    antenne.run()
