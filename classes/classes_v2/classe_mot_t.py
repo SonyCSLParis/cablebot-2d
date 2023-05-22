398b39d48a785969a4c642720275b32f58074f07
Arushivisuto <117741091+Arushivisuto@users.noreply.github.com>
2023-05-22 12:13:12 +0200

    Add files via upload

--- classes/classes_v2/classe_mot_t.py
+++ classes/classes_v2/classe_mot_t.py
@@ -121,6 +121,7 @@ class OdriveMot:
         self.cmax=cmax
         self.mode='v'
         self.tmax=tmax
+        self.tor=0.1
         #self.state=True
     
     def get(self):
@@ -146,6 +147,7 @@ class OdriveMot:
             ax.motor.config.pole_pairs = 7 # Nombre de paires de poles
             ax.motor.config.motor_type = od.MOTOR_TYPE_HIGH_CURRENT # Type de moteur
             ax.controller.config.vel_limit = 100 # Limite de vitesse (tr/min)
+            ax.controller.config.vel_rampe_rate = 2 #accéleration
 
             # Calibration du moteur et de l'encodeur
             print("4. AXIS_STATE_FULL_CALIBRATION_SEQUENCE")
@@ -186,7 +188,7 @@ class OdriveMot:
             self.odrv.axis0.controller.config.control_mode = 1
             self.mode=mod
             print("mode passé en couple")
-            self.odrv.axis0.controller.input_torque = 0.1
+            self.odrv.axis0.controller.input_torque = self.tor
             return 0
         else:
             print("Ceci n'est pas un mode valide")
